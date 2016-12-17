fe4de06ed0c9e43c4d86ee0f51d1f76a56da8b4a
michaelmachado <michael.e.machado@gmail.com>
2016-12-16 17:41:35 -0800

    Update index.js

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -32,15 +32,65 @@ exports.handle = (client) => {
       client.done()
     }
   })
+  
+  const handleGreeting = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addTextResponse('Hola Compandre')
+      client.done()
+    }
+  })
+
+  const handleGoodbye = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addTextResponse('Adios, Hope to see you soon!')
+      client.done()
+    }
+  })
+  
+  const collectProduct = client.createStep({
+  satisfied() {
+    return Boolean(client.getConversationState().productName)
+  },
+
+  prompt() {
+    // Need to prompt user for Product    
+    console.log('Prompt_product')
+    client.done()
+  },
+})
+  
+  const ProvideSignup = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt() {
+    // Need to provide Signup Instructions
+    console.log('PS_Docs')
+    client.done()
+  },
+})
 
   client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
+    goodbye: 'goodbye',
+    greeting: 'greeting'
     },
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
       end: [untrained],
